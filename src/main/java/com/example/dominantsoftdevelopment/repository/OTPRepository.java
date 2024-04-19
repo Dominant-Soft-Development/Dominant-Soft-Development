35ac8f42de7d16def447e000f56b1cf457ee669a
mamadiyev <mamad1yevvvv@gmail.com>
2024-04-19 17:16:31 +0500

    email o'chrilidi

--- src/main/java/com/example/dominantsoftdevelopment/repository/OTPRepository.java
+++ src/main/java/com/example/dominantsoftdevelopment/repository/OTPRepository.java
@@ -1,6 +1,5 @@
 package com.example.dominantsoftdevelopment.repository;
 
-import com.example.dominantsoftdevelopment.email.OTPEmail;
 import com.example.dominantsoftdevelopment.otp.OTP;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
@@ -9,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface OTPRepository extends CrudRepository<OTPEmail, String> {
-    Optional<OTPEmail> findByEmail(String email);
+public interface OTPRepository extends CrudRepository<OTP, String> {
+    Optional<OTP> findByEmail(String email);
 }
--- src/main/java/com/example/dominantsoftdevelopment/service/user_service/Userservice.java
+++ src/main/java/com/example/dominantsoftdevelopment/service/user_service/Userservice.java
@@ -1,12 +1,6 @@
 package com.example.dominantsoftdevelopment.service.user_service;
 
 import com.example.dominantsoftdevelopment.dto.*;
-import com.example.dominantsoftdevelopment.email.EmailCodeService;
-import com.example.dominantsoftdevelopment.email.OTPEmail;
-import com.example.dominantsoftdevelopment.email.dto.*;
-import com.example.dominantsoftdevelopment.exceptions.EmailAlreadyExistException;
-import com.example.dominantsoftdevelopment.exceptions.InvalidEmailAddressException;
-import com.example.dominantsoftdevelopment.exceptions.PasswordNotMatchException;
 import com.example.dominantsoftdevelopment.exceptions.RestException;
 import com.example.dominantsoftdevelopment.model.User;
 import com.example.dominantsoftdevelopment.model.enums.Status;
@@ -29,7 +23,6 @@ public class Userservice {
     private final ModelMapper modelMapper;
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
-    private final EmailCodeService emailService;
     private final OTPRepository otpRepository;
 
     @Transactional
@@ -62,49 +55,6 @@ public class Userservice {
 
         return userRepository.findAllByStatus(Status.ONLINE);
 
-    }
-
-
-    public UserResponseDto signIn(UserSignInDto signInDto) {
-        String password = signInDto.getPassword();
-        User user = repository
-                .findUserByEmail(signInDto.getEmail())
-                .orElseThrow(
-                        () -> new BadCredentialsException("Email or password incorrect")
-                );
-
-        if( !passwordEncoder.matches( password, user.getPassword() ) )
-        {
-            throw new BadCredentialsException( "Email or password doesn't match" );
-        }
-        return modelMapper.map(user , UserResponseDto.class);
-    }
-
-    @Transactional
-    public UserResponseDto signUp(UserCreateDto userCreateDto) {
-
-        String password = userCreateDto.getPassword();
-        String confirmPassword = userCreateDto.getConfirmPassword();
-        String email = userCreateDto.getEmail();
-
-        if (!emailService.isValid(userCreateDto.getEmail())){
-            throw new InvalidEmailAddressException("%s is not valid ".formatted(email));
-        }
-        if (repository.findUserByEmail(email).isPresent()){
-            throw new EmailAlreadyExistException("User with email %s already exist".formatted(email));
-        }
-        if (!password.equals(confirmPassword)){
-            throw new PasswordNotMatchException("Password and confirm password not matched!");
-        }
-
-        userCreateDto.setPassword(passwordEncoder.encode(userCreateDto.getPassword()));
-
-        emailService.sendEmail(email);
-
-        OTPEmail otp = modelMapper.map(userCreateDto, OTPEmail.class);
-        otpRepository.save(otp);
-
-        return modelMapper.map(userCreateDto, UserResponseDto.class);
     }
 }
 
